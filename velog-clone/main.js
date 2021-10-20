18f113b3cb3c91060799f0181e1210958fc6f1dc
jynam <njy1007@gmail.com>
2021-10-20 21:04:04 +0900

    feat: post 모달창 구현

--- velog-clone/main.js
+++ velog-clone/main.js
@@ -1,5 +1,10 @@
 const period = document.querySelector(".nav__period");
 const dropdown = document.querySelector(".nav__dropdown");
+const posts = document.querySelector(".posts");
+
+function onClickPeriod() {
+  dropdown.classList.toggle("visible");
+}
 
 function onClickDropdown(e) {
   dropdown.querySelectorAll("li").forEach(li => li.classList.remove("active"));
@@ -8,11 +13,32 @@ function onClickDropdown(e) {
 }
 
 function onClickPost(e) {
-  const post = e.target.closest("article");
-  console.dir(post);
-  console.log("event");
+  const clickedPost = e.target.closest("article");
+  // console.dir(clickedPost);
+
+  const background = document.createElement("div");
+  const modal = document.createElement("article");
+  const modalBtn = document.createElement("button");
+  
+  background.setAttribute("class", "post__modal-background");
+  modal.setAttribute("class", "post__modal");
+  modalBtn.setAttribute("class", "post__modal-btn material-icons");
+  modalBtn.innerText = "close";
+  modal.innerHTML = clickedPost.innerHTML;
+
+  background.appendChild(modal);
+  background.appendChild(modalBtn);
+  document.body.appendChild(background);
+
+  document.body.style.overflow = "hidden";
+  modalBtn.addEventListener("click", () => {
+    background.remove();
+    document.body.style.overflow = "visible";
+  });
 }
 
 period.addEventListener("click", onClickPeriod);
 dropdown.addEventListener("click", onClickPeriod);
 dropdown.addEventListener("click", onClickDropdown);
+
+posts.addEventListener("click", onClickPost);
