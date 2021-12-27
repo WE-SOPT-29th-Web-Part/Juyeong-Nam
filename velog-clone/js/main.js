e1daa407c927278ea631209d218b44f9e3ba0cdf
jynam <njy1007@gmail.com>
2021-12-27 17:51:00 +0900

    feat: 다크모드 구현

--- velog-clone/js/main.js
+++ velog-clone/js/main.js
@@ -1,15 +1,18 @@
-"use strict"
+"use strict";
 
 const period = document.querySelector(".nav__period");
 const dropdown = document.querySelector(".nav__dropdown");
 const posts = document.querySelector(".posts");
+const darkModeBtn = document.querySelector(".header__dark-mode-button input");
 
 function onClickPeriod() {
   dropdown.classList.toggle("visible");
 }
 
 function onClickDropdown(e) {
-  dropdown.querySelectorAll("li").forEach(li => li.classList.remove("active"));
+  dropdown
+    .querySelectorAll("li")
+    .forEach((li) => li.classList.remove("active"));
   e.target.classList.add("active");
   period.querySelector("span").innerText = e.target.innerHTML;
 }
@@ -20,7 +23,7 @@ function onClickPost(e) {
   const background = document.createElement("div");
   const modal = document.createElement("article");
   const modalBtn = document.createElement("button");
-  
+
   background.setAttribute("class", "post__modal-background");
   modal.setAttribute("class", "post__modal");
   modalBtn.setAttribute("class", "post__modal-btn material-icons");
@@ -38,8 +41,32 @@ function onClickPost(e) {
   });
 }
 
+function toggleColorTheme(e) {
+  if (e.target.checked) {
+    localStorage.setItem("colorTheme", "dark");
+    setColorTheme();
+  } else {
+    localStorage.setItem("colorTheme", "light");
+    setColorTheme();
+  }
+  console.log("삐약");
+}
+
+function setColorTheme() {
+  const colorTheme = localStorage.getItem("colorTheme");
+  if (colorTheme && colorTheme === "dark") {
+    document.body.classList.add("dark-theme");
+    darkModeBtn.checked = true;
+  } else {
+    document.body.classList.remove("dark-theme");
+    darkModeBtn.checked = false;
+  }
+}
+
 period.addEventListener("click", onClickPeriod);
 dropdown.addEventListener("click", onClickPeriod);
 dropdown.addEventListener("click", onClickDropdown);
-
 posts.addEventListener("click", onClickPost);
+darkModeBtn.addEventListener("click", toggleColorTheme);
+
+setColorTheme();
